8fd451d628cf18cc58d404161e108e998ddae646
icecream.zeng <zenghao2015xks@163.com>
2021-12-24 08:50:31 +0800

    netty

--- test/src/main/java/com/zh/test/concurrent/FutureTest.java
+++ test/src/main/java/com/zh/test/concurrent/FutureTest.java
@@ -1,7 +1,10 @@
 package com.zh.test.concurrent;
 
+import com.google.common.util.concurrent.*;
+
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
 
 /**
  * @Author: Zeng Hao
@@ -12,5 +15,49 @@ public class FutureTest {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         CompletableFuture<Integer> future = CompletableFuture.supplyAsync(() -> 1).thenApply(a -> a +1).thenApply(a -> a + 1);
         System.out.println(future.get());
+        ListeningExecutorService pool = MoreExecutors.listeningDecorator(Executors.newFixedThreadPool(10));
+
+        ListenableFuture listenableFuture = pool.submit(() ->{
+            for (int i = 0; i < 10; i++) {
+                System.out.println("listenableFuture : " + i);
+                Thread.sleep(1000);
+            }
+            return true;
+        });
+
+        Futures.addCallback(listenableFuture, new FutureCallback<Boolean>(){
+            @Override
+            public void onSuccess(Boolean o) {
+                System.out.println("return is " + o);
+                System.out.println(Thread.currentThread().getThreadGroup().activeCount());
+            }
+
+            @Override
+            public void onFailure(Throwable throwable) {
+                System.out.println("fail");
+            }
+        }, pool);
+
+        CompletableFuture.supplyAsync(() -> {
+            long start = System.currentTimeMillis();
+            for (int i = 0; i < 10; i++) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    Thread.interrupted();
+                }
+                System.out.println("CompletableFuture: " + i);
+
+            }
+            return System.currentTimeMillis() - start;
+        }).thenAccept(r -> {
+            System.out.println("runtime : " + r + " ms");
+            System.out.println(Thread.currentThread().getThreadGroup().activeCount());
+            pool.shutdown();
+            System.out.println("thread pool shutdown");
+        });
+
+
+        System.out.println("main thread end");
     }
 }
--- test/src/main/java/com/zh/test/concurrent/RunnableTest.java
+++ test/src/main/java/com/zh/test/concurrent/RunnableTest.java
@@ -10,17 +10,50 @@ public class RunnableTest {
     static int DELAY = 10;
     static int STEPS = 100;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
-        Runnable r = () -> {
-          try {
-              for (int i = 0; i < STEPS; i++) {
-                  Thread.sleep((long) (DELAY * Math.random()));
-
-              }
-          }catch (InterruptedException e){}
+//        Runnable r = () -> {
+//          try {
+//              for (int i = 0; i < STEPS; i++) {
+//                  Thread.sleep((long) (DELAY * Math.random()));
+//
+//              }
+//          }catch (InterruptedException e){}
+//        };
+//        r.run();
+//        Thread.currentThread().setDaemon(true);
+        Object o = new Object();
+        Runnable r1 = () -> {
+            try {
+                for (int i = 0; i < 20; i++) {
+                    System.out.println("r1:" + i);
+                    System.out.println(Thread.currentThread().getThreadGroup().activeCount());
+                    Thread.sleep(1000);
+                }
+            } catch (InterruptedException e) {
+                    e.printStackTrace();
+            }
         };
-        r.run();
-        Thread.currentThread().setDaemon(true);
+
+        Runnable r2 = () -> {
+            try {
+                for (int i = 0; i < 20; i++) {
+                    System.out.println("r2:" + i);
+                    System.out.println(Thread.currentThread().getThreadGroup().activeCount());
+                    Thread.sleep(1000);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
+
+        Thread t1 = new Thread(r1);
+        Thread t2 = new Thread(r2);
+
+        t1.setDaemon(true);
+        t2.setDaemon(true);
+        t1.start();
+        t2.start();
+        System.out.println("main end");
     }
 }
